1464f20a6fce362c50e62bf921333ec8c3e4d0d0
Lucas Donnian <LucasDCoder@gmail.com>
2022-07-24 13:42:25 -0300

    DesafioJS

---
+++ login.js
@@ -0,0 +1,15 @@
+function login(){
+    var usuario = document.getElementById("user").value
+    var password = document.getElementById("password").value
+
+    let user = "prueba"
+    let pass = "12345"
+
+    if(usuario==user && password==12345){
+        alert("Usuario correcto")
+        document.location.href = "index.html"
+    }else{
+        alert("Usuario o contraseña incorrecto")
+        document.location.href = "Login.html"
+    }
+}
