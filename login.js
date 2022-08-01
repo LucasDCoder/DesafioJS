3d75b5caa3fe6c1bd812e6d8541a8d95a055d854
Lucas Donnian <LucasDCoder@gmail.com>
2022-08-01 15:44:09 -0300

    actualizacion

--- login.js
+++ login.js
@@ -1,8 +1,4 @@
-function login(){
-    let user = document.getElementById("user").value
-    let password = document.getElementById("password").value
-
-    let usuarios = [
+    const usuarios = [
         {
             user: "lucas",
             password: "1234"
@@ -16,6 +12,10 @@ function login(){
             password: "4321"
         }
     ]
+    
+    function login(){
+        let user = document.getElementById("user").value
+        let password = document.getElementById("password").value
 
     for(i = 0; i < usuarios.length; i++){
         if(user == usuarios[i].user && password == usuarios[i].password){
@@ -27,3 +27,23 @@ function login(){
     alert("Usuario o contraseña incorrecto")
     document.location.href = "index.html"
 }
+
+function registro(){
+    let registroUsuario = document.getElementById("nuevoUsuario").value
+    let registroPassword = document.getElementById("nuevaPassword").value
+    let nuevoUsuario = {
+        user: registroUsuario,
+        password: registroPassword
+    }
+    for(i = 0; i < usuarios.length; i++){
+        if(registroUsuario == usuarios[i].user){ 
+            alert("El nombre ya está registrado, por favor elija otro")
+            return
+        }
+    }
+    
+    usuarios.push(nuevoUsuario)
+    alert("Registro Exitoso")
+    console.log(usuarios);
+}
+
